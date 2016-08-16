91a1b5c867311641a1dd1cb54e3307d00fbeca21
Joy Harvel <geekgirl.joy@gmail.com>
2016-08-15 23:25:52 -0700

    Adding Voxel.cs and README.md

---
+++ Loose Code/Voxel.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum VoxelType{
+	Sphere, Capsule, Cylinder, Cube, Plane, Quad
+}
+
+public class Voxel : MonoBehaviour {
+
+	private GameObject myVoxel;
+	public VoxelType voxelType;
+    public Material voxelMaterial;
+	public Vector3 position;
+	public bool breakable = false;
+	public int blockHealth = 50;
+	
+
+	void Start() {
+		// Create this Voxel based on type
+		if(voxelType == VoxelType.Sphere) {
+			myVoxel = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		}
+		else if(voxelType == VoxelType.Capsule) {
+			myVoxel = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+		}
+		else if(voxelType == VoxelType.Cylinder) {
+			myVoxel = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		}
+		else if(voxelType == VoxelType.Cube) {
+			myVoxel = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		}
+		else if(voxelType == VoxelType.Plane) {
+			myVoxel = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		}
+		else if(voxelType == VoxelType.Quad) {
+			myVoxel = GameObject.CreatePrimitive(PrimitiveType.Quad);
+		}
+		myVoxel.GetComponent<Renderer>().material = voxelMaterial;
+		myVoxel.transform.position = position;
+
+	}
+		
+	void Update() {
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# PHP
+Examples of my C# Code.
+
+Essentially this repository is an ever growing backup of all my public C#. It represents a living body of work and I can, will and do update it frequently.
+
+##C# Projects
+
+
+##Loose Code
+This folder contains stand alone code. Each does domething different and is here because I want a backup of this code for any number of reasons.                                
+######[Voxel.cs](Loose Code/Voxel.cs)
+
+
+All of this code is subject to minor alterations, complete rewrites, even removal.
